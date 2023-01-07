0c35f189eb71af065dc085e1677a089a384881e9
frusanov <f.rusanov@gmail.com>
2023-01-07 12:00:39 +0000

    Deploying to gh-pages from @ frusanov/feodor.me@7f1f7d4681bb37b3149e8a020594f6ab39ce59b5 🚀

---
+++ _next/static/cEEULkdy4egjLFo_UGO8a/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f42cc0a839d49e25.js"],"/_error":["static/chunks/pages/_error-4b61be14865575c5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
